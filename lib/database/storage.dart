b766a8a9a186993e55c34b0bec6ad1db9e1d4795
Arjun-P-Jayakrishnan <arjunpjayakrishnan@gmail.com>
2022-11-19 23:08:24 +0530

    commit 2

--- lib/database/storage.dart
+++ lib/database/storage.dart
@@ -1,10 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:farm_hack/pages/homepage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import '../pages/homepage.dart';
 
-Future<void> storage() async {
-  await FirebaseFirestore.instance
-      .collection('groceries')
-      .add({'Name': "tomato", 'Price': "12"});
+
+
+
+class storage extends StatelessWidget {
+  const storage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future:Firebase.initializeApp(),
+      builder: (context,snapshot){
+        if(snapshot.hasError){
+          return Text('Error');
+        }
+         if(snapshot.connectionState==ConnectionState.done){
+           return Homepage();
+         }
+        return Text('Loading');
+      },
+    );
+  }
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'LoginPage.dart';
+import 'package:farm_hack/pages/homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -10,25 +16,92 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+
+
   var vegetables = ['tomato', 'carrot', 'rice', 'potato', 'cereals'];
 
   @override
+  void initState() {
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: 5,
-          itemBuilder: (BuildContext context, int index) {
-            return GestureDetector(
-              child: ListTile(
-                  leading: Image.asset('assets/${vegetables[index]}.png'),
-                  trailing: const Text(
-                    "Price",
-                    style: TextStyle(color: Colors.green, fontSize: 15),
-                  ),
-                  title: Text("${vegetables[index]}")),
-              onTap: () => {Navigator.of(context).pushNamed(LoginPage.id)},
-            );
-          }),
+  //     body:FutureBuilder(
+  //       future:getData(),
+  //       builder:(context,AsyncSnapshot<DocumentSnapshot> snapshot){
+  //         if(snapshot.connectionState==ConnectionState.done){
+  //             return ListView.builder(
+  //                 itemCount: 5,
+  //                 itemBuilder: (BuildContext context, int index) {
+  //                   return GestureDetector(
+  //                     child: ListTile(
+  //                         leading: Text(''),
+  //                         trailing: const Text(
+  //                           "Price",
+  //                           style: TextStyle(color: Colors.green, fontSize: 15),
+  //                         ),
+  //                         title: Text("${vegetables[index]}")),
+  //                     onTap: () => {Navigator.of(context).pushNamed(LoginPage.id)},
+  //                   );
+  //                 });
+  //         }
+  //         else if(snapshot.connectionState==ConnectionState.none){
+  //           return Text("No connection");
+  //         }
+  //         return CircularProgressIndicator();
+  //       },
+  //   )
+  //   );
+  // }
+     body:StreamBuilder(
+       stream:FirebaseFirestore.instance.collection("groceries").snapshots(),
+       builder:(context,AsyncSnapshot<QuerySnapshot> snapshot){
+
+        if(snapshot.connectionState==ConnectionState.done){
+                    return FirestoreListView(snapshot.data.docs);
+                }
+                else if(snapshot.connectionState==ConnectionState.none){
+                  return Text("No connection");
+                }
+                return CircularProgressIndicator();
+
+       },
+     ));
+  
+  
+
+  Future<DocumentSnapshot> getData() async {
+    await Firebase.initializeApp();
+    return await FirebaseFirestore.instance
+        .collection("groceries")
+        .doc("vegetables")
+        .get();
+  }
+
+}
+}
+
+class FirestoreListView extends StatelessWidget {
+  FirestoreListView(this.documents);
+  final List<DocumentSnapshot> documents;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: documents.length,
+      itemExtent: 90.0,
+      itemBuilder: (BuildContext context, int index){
+
+        String name = documents[index].data["title"].toString();
+
+        return Text(name);
+
+      },
     );
   }
 }
+
+
