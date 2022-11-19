63387ef79292deda31ec87cd58e132ad745abb99
Arjun-P-Jayakrishnan <arjunpjayakrishnan@gmail.com>
2022-11-20 00:55:28 +0530

    commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import './pages/homepage.dart';
 import './pages/LoginPage.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SharedPreferences prefs = await SharedPreferences.getInstance();
   var email = prefs.getString("email");
   print(email);
